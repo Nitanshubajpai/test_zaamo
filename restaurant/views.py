f2ce82138d7f67192075fc5e520d8f0ede9e1f5e
Nitanshu bajpai <nitanshubajpai@gmail.com>
2021-12-11 11:44:47 +0530

    update

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,5 @@
 from django.http import response
 from django.shortcuts import render
-from rest_framework.fields import CurrentUserDefault
 from rest_framework.views import APIView, Response
 from django.http import JsonResponse
 from restaurant.models import restaurant
@@ -14,11 +13,11 @@ class Restaurant(APIView):
             objects = restaurant.objects.filter(type=type)
             response = []
             for i in objects:
-                currect_object = {}
-                currect_object["name"]=i.name
-                currect_object["type"]=i.type
-                currect_object["description"]=i.description
-                response.append(currect_object)
+                current_object = {}
+                current_object["name"]=i.name
+                current_object["type"]=i.type
+                current_object["description"]=i.description
+                response.append(current_object)
             return JsonResponse({"data":response})
         except Exception as e:
             return JsonResponse({"data":e})
